90779bd9454692c24865e7d773e57b59b92bbf64
theorllr <theorllr@gmail.com>
2026-07-18 17:26:24 +0200

    Correct text in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,1 @@
-export function Footer(){return <footer><div className="footer-mark">SHINEMA</div><p>Être curieux,<br/>plus qu'un vilain défaut.</p><span className="copyright">© 2026 — VERSION 1.0</span></footer>}
+export function Footer(){return <footer><div className="footer-mark">SHINEMA</div><p>ÊTRE CURIEUX.</p><span className="copyright">© 2026 — VERSION 1.0</span></footer>}
